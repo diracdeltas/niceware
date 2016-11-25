f7b1411ff17cf5804a2205cfa94026ccd5d50127
yan <yan@mit.edu>
2016-11-24 16:47:04 -0800

    switch to Tape, add browser tests
    fix #1

--- test/mainTest.js
+++ test/mainTest.js
@@ -1,6 +1,4 @@
-/* global describe, it */
-
-const assert = require('assert')
+const test = require('tape')
 const niceware = require('../lib/main')
 
 function stringify (array) {
@@ -11,37 +9,43 @@ function arrayify (string) {
   return string.split(' ')
 }
 
-describe('generatePassphrase', function () {
-  it('generates passphrases of the correct length', function () {
-    assert.equal(1, niceware.generatePassphrase(2).length)
-    assert.equal(0, niceware.generatePassphrase(0).length)
-    assert.equal(10, niceware.generatePassphrase(20).length)
-    assert.equal(256, niceware.generatePassphrase(512).length)
+test('generatePassphrase', function (t) {
+  t.test('generates passphrases of the correct length', function (q) {
+    q.plan(4)
+    q.equal(1, niceware.generatePassphrase(2).length)
+    q.equal(0, niceware.generatePassphrase(0).length)
+    q.equal(10, niceware.generatePassphrase(20).length)
+    q.equal(256, niceware.generatePassphrase(512).length)
   })
-  it('throws when generating passphrase with an odd number of bytes', function () {
-    assert.throws(niceware.generatePassphrase.bind(null, 1), /even/)
-    assert.throws(niceware.generatePassphrase.bind(null, 23), /even/)
+  t.test('throws when generating passphrase wt.testh an odd number of bytes', function (q) {
+    q.plan(2)
+    q.throws(niceware.generatePassphrase.bind(null, 1), /even/)
+    q.throws(niceware.generatePassphrase.bind(null, 23), /even/)
   })
-  it('throws when generating passphrase with >1024 or <0 bytes', function () {
-    assert.throws(niceware.generatePassphrase.bind(null, 1026), /1024/)
-    assert.throws(niceware.generatePassphrase.bind(null, -4), /1024/)
+  t.test('throws when generating passphrase wt.testh >1024 or <0 bytes', function (q) {
+    q.plan(2)
+    q.throws(niceware.generatePassphrase.bind(null, 1026), /1024/)
+    q.throws(niceware.generatePassphrase.bind(null, -4), /1024/)
   })
 })
 
-describe('bytesToPassphrase', function () {
-  it('throws when byte array has odd length', function () {
-    assert.throws(niceware.bytesToPassphrase.bind(null, Buffer.alloc(1)), /even/)
+test('bytesToPassphrase', function (t) {
+  t.test('throws when byte array has odd length', function (q) {
+    q.plan(1)
+    q.throws(niceware.bytesToPassphrase.bind(null, Buffer.alloc(1)), /even/)
   })
-  it('throws when input is not a byte array', function () {
-    assert.throws(niceware.bytesToPassphrase.bind(null, [1, 1]), /Buffer/)
+  t.test('throws when input is not a byte array', function (q) {
+    q.plan(1)
+    q.throws(niceware.bytesToPassphrase.bind(null, [1, 1]), /Buffer/)
   })
-  it('returns expected passphrases', function () {
-    assert.equal(0, niceware.bytesToPassphrase(Buffer.alloc(0)).length)
-    assert.equal('a',
+  t.test('returns expected passphrases', function (q) {
+    q.plan(4)
+    q.equal(0, niceware.bytesToPassphrase(Buffer.alloc(0)).length)
+    q.equal('a',
       stringify(niceware.bytesToPassphrase(Buffer.from([0x00, 0x00]))))
-    assert.equal('zyzzyva',
+    q.equal('zyzzyva',
       stringify(niceware.bytesToPassphrase(Buffer.from([0xff, 0xff]))))
-    assert.equal(
+    q.equal(
       'a bioengineering balloted gobbledegook creneled written depriving zyzzyva',
       stringify(niceware.bytesToPassphrase(Buffer.from(
         [0, 0, 17, 212, 12, 140, 90, 247, 46, 83, 254, 60, 54, 169, 255, 255])))
@@ -49,23 +53,26 @@ describe('bytesToPassphrase', function () {
   })
 })
 
-describe('passphraseToBytes', function () {
-  it('throws when input is invalid', function () {
-    assert.throws(niceware.passphraseToBytes.bind(null, Buffer.alloc(1)), /array/)
-    assert.throws(niceware.passphraseToBytes.bind(null, [1, 2]), /string/)
+test('passphraseToBytes', function (t) {
+  t.test('throws when input is invalid', function (q) {
+    q.plan(2)
+    q.throws(niceware.passphraseToBytes.bind(null, Buffer.alloc(1)), /array/)
+    q.throws(niceware.passphraseToBytes.bind(null, [1, 2]), /string/)
   })
-  it('throws when input is not in the wordlist', function () {
-    assert.throws(niceware.passphraseToBytes.bind(null, ['i', 'love', 'ninetales']),
-      /passphrase/)
+  t.test('throws when input is not in the wordlist', function (q) {
+    q.plan(1)
+    q.throws(niceware.passphraseToBytes.bind(null, ['You', 'love', 'ninetales']),
+      /ninetales/)
   })
-  it('returns expected bytes', function () {
-    assert(Buffer.from([0x00, 0x00]).equals(
+  t.test('returns expected bytes', function (q) {
+    q.plan(3)
+    q.ok(Buffer.from([0x00, 0x00]).equals(
       niceware.passphraseToBytes(arrayify('A'))
     ))
-    assert(Buffer.from([0xff, 0xff]).equals(
+    q.ok(Buffer.from([0xff, 0xff]).equals(
       niceware.passphraseToBytes(arrayify('zyzzyva'))
     ))
-    assert(Buffer.from([0, 0, 17, 212, 12, 140, 90, 247, 46, 83, 254, 60, 54, 169, 255, 255]).equals(
+    q.ok(Buffer.from([0, 0, 17, 212, 12, 140, 90, 247, 46, 83, 254, 60, 54, 169, 255, 255]).equals(
       niceware.passphraseToBytes(arrayify('A bioengineering Balloted gobbledegooK cReneled Written depriving zyzzyva'))
     ))
   })
